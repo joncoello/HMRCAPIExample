3826f249be0f95181e42735cb24250e5faaec9b3
Jon Coello <joncoello@hotmail.com>
2016-04-14 13:42:53 +0100

    Add project files.

---
+++ HMRCAPIExample/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace HMRCAPIExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdHelloWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdHelloWorld
+            // 
+            this.cmdHelloWorld.Location = new System.Drawing.Point(421, 434);
+            this.cmdHelloWorld.Name = "cmdHelloWorld";
+            this.cmdHelloWorld.Size = new System.Drawing.Size(131, 37);
+            this.cmdHelloWorld.TabIndex = 0;
+            this.cmdHelloWorld.Text = "Hello World";
+            this.cmdHelloWorld.UseVisualStyleBackColor = true;
+            this.cmdHelloWorld.Click += new System.EventHandler(this.cmdHelloWorld_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(564, 568);
+            this.Controls.Add(this.cmdHelloWorld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdHelloWorld;
+    }
+}
+
---
+++ HMRCAPIExample/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMRCAPIExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdHelloWorld_Click(object sender, EventArgs e)
+        {
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.service.hmrc.gov.uk");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.hmrc.1.0+json"));
+            
+            var response = client.GetAsync("/hello/world").Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            MessageBox.Show(content);
+
+        }
+    }
+}
